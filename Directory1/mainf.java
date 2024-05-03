20bd30dc06daa5fb2edeb41d2ba1523d6376161c
Factorial343 <107515235+Factorial343@users.noreply.github.com>
2024-05-03 13:29:04 -0400

    Update mainf.java

--- Directory1/mainf.java
+++ Directory1/mainf.java
@@ -7,12 +7,11 @@ public class mainf {
 	public static void main(String[] args) throws IOException {
 		// TODO import java.io.*;
 		 // Display home screen
-        System.out.println("Welcome to Java Console Commands!");
-        System.out.println("You can use one of the following commands:");
+        System.out.println("Welcome to File Management Simulation in Java!");
+        System.out.println("You can explore File Management using the following commands:");
         System.out.println("- touch");
         System.out.println("- mkdir");
         System.out.println("- rm");
-        System.out.println("- cd");
         System.out.println("- mv");
         System.out.println("- cp");
         System.out.println("- cp -r");
@@ -39,16 +38,16 @@ public class mainf {
 		  new1.mkfil();
       } else if (command.equals("mkdir")) {
           System.out.println("You have selected the 'mkdir' command.");
-          CreateFilDir new2 = new CreateFilDir();
-          new2.mkdir();
+          CreateFilDir new1 = new CreateFilDir();
+          new1.mkdir();
       } else if (command.equals("rm")) {
           System.out.println("You have selected the 'rm' command.");
-          CreateFilDir new3 = new CreateFilDir();
-          new3.delfil();
+          CreateFilDir new1 = new CreateFilDir();
+          new1.delfil();
       }  else if (command.equals("mv")) {
           System.out.println("You have selected the 'mv' command.");
-          CreateFilDir new3 = new CreateFilDir();
-          new3.mv();
+          CreateFilDir new1 = new CreateFilDir();
+          new1.mv();
       } else if (command.equals("cp")) {
           System.out.println("You have selected the 'cp' command.");
           File copy1 = new File("C:\\Users\\KarlfromAllState\\eclipse-workspace\\439Proj\\Dir1\\SpecificFile.txt");
@@ -63,20 +62,20 @@ public class mainf {
           System.out.println("The contents of folder have been transsferred to 439 Proj");
       } else if (command.equals("ls")) {
           System.out.println("You have selected the 'ls' command.");
-          CreateFilDir new11 = new CreateFilDir();
-          new11.ls();
+          CreateFilDir new1 = new CreateFilDir();
+          new1.ls();
       } else if (command.equals("pwd")) {
           System.out.println("You have selected the 'pwd' command.");
-          CreateFilDir new9 = new CreateFilDir();
-          new9.pwd();
+          CreateFilDir new1 = new CreateFilDir();
+          new1.pwd();
       }  else if (command.equals("renamef")) {
     	  System.out.println("You have selected the 'renamef' command.");
-          CreateFilDir new11 = new CreateFilDir();
-    	  new11.renamefil();
+          CreateFilDir new1 = new CreateFilDir();
+    	  new1.renamefil();
       } else if (command.equals("size")) {
         	  System.out.println("You have selected the 'size' command.");
-              CreateFilDir new12 = new CreateFilDir();
-        	  new12.size();
+              CreateFilDir new1 = new CreateFilDir();
+        	  new1.size();
       } else if (command.equals("cat")) {
           System.out.println("You have selected the 'cat' command.");
           CreateFilDir new1 = new CreateFilDir();
